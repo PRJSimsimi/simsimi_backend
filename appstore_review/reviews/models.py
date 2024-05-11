0bf73a1f95887ec6ae37aee533338dcee8d479fe
LeeDuho <dl6874@gmail.com>
2024-05-11 09:01:12 +0900

    customer_reviews model 에 selected_response 추가

--- appstore_review/reviews/models.py
+++ appstore_review/reviews/models.py
@@ -3,13 +3,16 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class customer_reviews(models.Model):
     id = models.CharField(max_length=255, primary_key=True)  # 리뷰 고유 ID
-    # rating = models.IntegerField()  # 별점 (1~5)
-    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])  # 별점 (1~5)
+    rating = models.IntegerField()  # 별점 (1~5)
+    # rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])  # 별점 (1~5)
     reviewer_nickname = models.CharField(max_length=255)  # 작성자 닉네임
     title = models.CharField(max_length=255)  # 리뷰 제목
     body = models.TextField()  # 리뷰 본문
     created_date = models.DateTimeField()  # 리뷰 작성일
     territory = models.CharField(max_length=3)  # 지역 코드 (예: KOR, USA)
+
+    selected_response = models.TextField(null=True, blank=True)  # 선택된 답변
+    
 
     class Meta:
         db_table = 'customer_reviews'  # 데이터베이스 테이블 이름
