e10b4df190cbdb4880f74cdd9da9bc42c7f9950a
LeeDuho <dl6874@gmail.com>
2024-05-24 11:49:33 +0900

    model 필요 없는 코드 삭제

--- appstore_review/appstore_review/celery.py
+++ appstore_review/appstore_review/celery.py
@@ -3,6 +3,7 @@ import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appstore_review.settings')
+
 app = Celery('appstore_review')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
--- appstore_review/reviews/models.py
+++ appstore_review/reviews/models.py
@@ -4,7 +4,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class customer_reviews(models.Model):
     id = models.CharField(max_length=255, primary_key=True)  # 리뷰 고유 ID
     rating = models.IntegerField()  # 별점 (1~5)
-    # rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])  # 별점 (1~5)
     reviewer_nickname = models.CharField(max_length=255)  # 작성자 닉네임
     title = models.CharField(max_length=255)  # 리뷰 제목
     body = models.TextField()  # 리뷰 본문
@@ -16,6 +15,3 @@ class customer_reviews(models.Model):
 
     class Meta:
         db_table = 'customer_reviews'  # 데이터베이스 테이블 이름
-
-    # def __str__(self):
-    #     return f"{self.reviewer_nickname} - {self.title[:20]}"
